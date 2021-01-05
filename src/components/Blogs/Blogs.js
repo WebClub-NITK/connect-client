439fb1c7397c80dbf485122a3536f31dc1578b10
Ashwin p <ashwinprasanna9@gmail.com>
2021-01-05 19:13:50 +0530

    blogs landing page done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,25 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import BlogTile from './BlogTile'
-import { getAllBlogs } from '../../services/blogsService'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import BlogTile from "./BlogTile";
+import { getAllBlogs } from "../../services/blogsService";
+import { Link } from "react-router-dom";
+import "./Blogs.css";
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
 
-    useEffect(async () => {
-        const blogs = await getAllBlogs()
-        setBlogs(blogs)
-    }, [])
+  useEffect(async () => {
+    const blogs = await getAllBlogs();
+    setBlogs(blogs);
+  }, []);
 
-    return (
-        <div>
-            <h1>Blogs</h1>
-            <Link to='/blogs/new'>
-                <button>Share an Idea</button>
-            </Link>
-            {blogs ? blogs.map(blog => <BlogTile key={blog._id} details={blog} />) : <p>No blogs to display</p>}
-        </div>
-    )
-}
+  function getDescription(blog) {
+    const data = JSON.parse(blog.body);
+    return data.blocks;
+  }
 
-export default Blogs
+  const handleClick = (e) => {
+   e.preventDefault();
+  }
+
+  return (
+    <div>
+      <header className="nav_blogs">
+        <h2 className="title">Blogs</h2>
+        <form onSubmit={handleClick}>
+          <input className='search_input' placeholder='search for blogs'></input>
+          <button className='search_button' type='submit'>search</button>
+        </form>
+      </header>
+      <Link to="/blogs/new">
+        <button>Share an Idea</button>
+      </Link>
+      {blogs ? (
+        blogs.map((blog) => (
+          <BlogTile
+            key={blog._id}
+            details={blog}
+            description={getDescription(blog)}
+          />
+        ))
+      ) : (
+        <p>No blogs to display</p>
+      )}
+    </div>
+  );
+};
+
+export default Blogs;
