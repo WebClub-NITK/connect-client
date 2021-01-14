0ba28fc057e0dd0ba67418dc1c49d719bf653d06
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-01-14 18:53:42 +0530

    handling junk images in blogs editor

--- src/components/Blogs/NewBlog.js
+++ src/components/Blogs/NewBlog.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 import EditorJs from 'react-editor-js';
 import {tools} from './editorConfig'
 import {saveBlog} from '../../services/blogsService'
@@ -7,9 +7,7 @@ import styles from './blogStyles'
 
 const NewBlog = () => {
     let history = useHistory();
-    const [title, setTitle] = useState('')
-    const [tags, setTags] = useState('')
-
+    
     const instanceRef = useRef(null)
 
     const getBody = async () => {
@@ -17,14 +15,49 @@ const NewBlog = () => {
         return savedData
     }
 
+    useEffect(() => {
+        window.addEventListener('beforeunload', alertUser)
+        window.addEventListener('unload', cleanUp)
+        return () => {
+            window.removeEventListener('beforeunload', alertUser)
+            window.removeEventListener('unload', cleanUp)
+            cleanUp()
+        }
+    }, [])
+    const alertUser = async e => {
+        const body = await getBody()
+        let urls = []
+        body.blocks.forEach(block => {
+            if(block.type && block.type === 'image'){
+                if(block.data.file.from_server){
+                    urls.push(block.data.file.url)
+                }
+            }
+        });
+        if (urls.length !== 0) localStorage.setItem('imagestodelete', JSON.stringify(urls))
+        e.preventDefault()
+        e.returnValue = ''
+    }
+    const cleanUp = () => {
+        const urls = JSON.parse(localStorage.getItem('imagestodelete'))
+
+        if(urls !== null) {
+            fetch('http://localhost:3001/blogs/remove_images', {
+                method: 'POST',
+                body: JSON.stringify({images: urls}),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            })
+        }
+        localStorage.removeItem('imagestodelete')
+    }
+
     const handleClick = async () => {
         const title = document.getElementById('title').value
         const tags = document.getElementById('tags').value.split(',')
         const body = await getBody()
         const response = await saveBlog({title, body, tags})
-        console.log(response)
-        console.log(title, body)
-        alert('blog saved')
         history.push(`/blogs/${response._id}`)
     }
 
@@ -51,7 +84,4 @@ const NewBlog = () => {
 }
 
 export default NewBlog
-
-
-
 
