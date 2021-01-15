60568106cc5d4ff4d588dd6f2197d1311399411c
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-01-15 12:40:41 +0530

    handling unused image uploads in blogs editor

--- src/components/Blogs/NewBlog.js
+++ src/components/Blogs/NewBlog.js
@@ -1,20 +1,47 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import EditorJs from 'react-editor-js';
 import {tools} from './editorConfig'
 import {saveBlog} from '../../services/blogsService'
 import {useHistory} from 'react-router-dom'
 import styles from './blogStyles'
+import {Prompt} from 'react-router-dom'
 
 const NewBlog = () => {
     let history = useHistory();
+
+    // store image urls for images sent to the server
+    let imageUrls = []
     
     const instanceRef = useRef(null)
 
+    // function to get editor data
     const getBody = async () => {
         const savedData = await instanceRef.current.save()
         return savedData
     }
 
+    // function to handle image upload by file
+    const handleUploadByFile = (file) => {
+        // send the image to server which returns a public url after storing
+        const formData = new FormData();
+        formData.append('image', file);
+        let options = {
+            method: 'POST',
+            body: formData,
+        }
+        return fetch('http://localhost:3001/blogs/file_image_upload', options)
+        .then(res => res.json()).then(data => {
+            if(data.success) imageUrls.push(data.file.url)
+            return data
+        })
+    }
+
+    // add the image upload method to the editor tool
+    tools.image.config.uploader = {
+        uploadByFile: handleUploadByFile
+    }
+
+    // to ask for confirmation before leaving website
     useEffect(() => {
         window.addEventListener('beforeunload', alertUser)
         window.addEventListener('unload', cleanUp)
@@ -25,44 +52,57 @@ const NewBlog = () => {
         }
     }, [])
     const alertUser = async e => {
-        const body = await getBody()
-        let urls = []
-        body.blocks.forEach(block => {
-            if(block.type && block.type === 'image'){
-                if(block.data.file.from_server){
-                    urls.push(block.data.file.url)
-                }
-            }
-        });
-        if (urls.length !== 0) localStorage.setItem('imagestodelete', JSON.stringify(urls))
         e.preventDefault()
         e.returnValue = ''
     }
-    const cleanUp = () => {
-        const urls = JSON.parse(localStorage.getItem('imagestodelete'))
 
-        if(urls !== null) {
-            fetch('http://localhost:3001/blogs/remove_images', {
+    // delete all the unused images from the server
+    const cleanUp = async () => {
+        // sends a list of urls to the server which then deletes those images.
+        if(imageUrls.length !== 0) {
+            await fetch('http://localhost:3001/blogs/remove_images', {
                 method: 'POST',
-                body: JSON.stringify({images: urls}),
+                body: JSON.stringify({images: imageUrls}),
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8'
                 }
             })
         }
-        localStorage.removeItem('imagestodelete')
     }
 
-    const handleClick = async () => {
+    const handleSubmitPost = async () => {
         const title = document.getElementById('title').value
         const tags = document.getElementById('tags').value.split(',')
         const body = await getBody()
+        // image urls used in the post
+        let usedImageUrls = []
+        body.blocks.forEach(block => {
+            if(block.type && block.type === 'image'){
+                if(block.data.file.from_server){
+                    usedImageUrls.push(block.data.file.url)
+                }
+            }
+        });
+
+        for (const url of usedImageUrls){
+            imageUrls.splice(url, 1)
+        }
+
+        // clean the unused images
+        cleanUp()
         const response = await saveBlog({title, body, tags})
         history.push(`/blogs/${response._id}`)
     }
 
     return (
         <div>
+            <Prompt
+                message={async (location, action) => {
+                    // remove unused images if user navigates to a different route.
+                    await cleanUp()
+                    return true
+                }}
+            />
             <div style={{background:'lightgray', padding: '50px'}}>
                 <div style={{width:'50vw', margin: '10px auto', padding: '50px', background: 'white', borderRadius: '10px'}}>
                     <div style={{}}>
@@ -76,7 +116,7 @@ const NewBlog = () => {
                         placeholder='Share something interesting!'
                         logLevel='WARN'
                     />
-                    <button onClick={handleClick} style={styles.saveBlogButton}>Post</button>
+                    <button onClick={handleSubmitPost} style={styles.saveBlogButton}>Post</button>
                 </div>
             </div>
         </div>
